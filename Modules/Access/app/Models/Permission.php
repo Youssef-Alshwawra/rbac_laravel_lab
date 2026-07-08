506aace4fec4299bcce94f956e5de98dbbdaf053
shwawra <yousefkhaled7763@gmail.com>
2026-07-08 15:49:41 +0300

    Refactor Permission model docblock and fix roles method return statement

--- Modules/Access/app/Models/Permission.php
+++ Modules/Access/app/Models/Permission.php
@@ -8,6 +8,26 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 // use Modules\Access\Database\Factories\PermissionFactory;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Permission newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Permission newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Permission query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Permission whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Permission whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Permission whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Permission whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Permission whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Permission whereUpdatedAt($value)
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Modules\Access\Models\Role> $roles
+ * @property-read int|null $roles_count
+ * @mixin \Eloquent
+ */
 class Permission extends Model
 {
     use HasFactory;
@@ -23,6 +43,6 @@ class Permission extends Model
     // }
 
     public function roles(): BelongsToMany { 
-        $this->belongsToMany(Role::class, 'role_permission', 'permission_id', 'role_id')->withTimestamps();
+        return $this->belongsToMany(Role::class, 'role_permission', 'permission_id', 'role_id')->withTimestamps();
     }
 }   
